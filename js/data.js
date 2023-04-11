33cdce89b8dbe04a9d1463b465c431106348057c
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-11 21:58:57 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,70 +3,70 @@ const qnaList = [
     q: '1. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 금', score: 1 },
-      { answer: 'b. 도지코인', score: 9 },
+      { answer: 'b. 도지코인', score: 9 }
     ]
   },
   {
     q: '2. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 테슬라', score: 8 },
+      { answer: 'b. 테슬라', score: 8 }
     ]
   },
   {
     q: '3. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 나스닥', score: 5 },
-      { answer: 'b. 비트코인', score: 7 },
+      { answer: 'b. 비트코인', score: 7 }
     ]
   },
   {
     q: '4. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 금', score: 1 },
-      { answer: 'b. 유가', score: 6 },
+      { answer: 'b. 유가', score: 6 }
     ]
   },
   {
     q: '5. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 삼성전자', score: 4 },
-      { answer: 'b. 나스닥', score: 5 },
+      { answer: 'b. 나스닥', score: 5 }
     ]
   },
   {
     q: '6. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 코스피', score: 3 },
-      { answer: 'b. 삼성전자', score: 4 },
+      { answer: 'b. 삼성전자', score: 4 }
     ]
   },
   {
     q: '7. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 코스피', score: 3 },
+      { answer: 'b. 코스피', score: 3 }
     ]
   },
   {
     q: '8. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 코스피', score: 3 },
+      { answer: 'b. 코스피', score: 3 }
     ]
   },
   {
     q: '9. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 코스피', score: 3 },
+      { answer: 'b. 코스피', score: 3 }
     ]
   },
   {
     q: '10. 둘 중 하나만 투자 가능하다면?',
     a: [
       { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 코스피', score: 3 },
+      { answer: 'b. 코스피', score: 3 }
     ]
   }
 ]
