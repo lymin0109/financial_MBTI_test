d5eae3ed92cd7629723d6a76bec0138abc22c09c
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-11 23:54:15 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,43 +2,43 @@ const qnaList = [
   {
     q: '1. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 금', score: 1 },
-      { answer: 'b. 도지코인', score: 9 }
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 도지코인', score: 5 }
     ]
   },
   {
     q: '2. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 미국 국채', score: 2 },
-      { answer: 'b. 테슬라', score: 8 }
+      { answer: 'a. 미국 국채', score: 0 },
+      { answer: 'b. 테슬라', score: 5 }
     ]
   },
   {
     q: '3. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 나스닥', score: 5 },
-      { answer: 'b. 비트코인', score: 7 }
+      { answer: 'a. 나스닥', score: 0 },
+      { answer: 'b. 비트코인', score: 5 }
     ]
   },
   {
     q: '4. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 금', score: 1 },
-      { answer: 'b. 유가', score: 6 }
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 유가', score: 5 }
     ]
   },
   {
     q: '5. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 삼성전자', score: 4 },
+      { answer: 'a. 삼성전자', score: 0 },
       { answer: 'b. 나스닥', score: 5 }
     ]
   },
   {
     q: '6. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 코스피', score: 3 },
-      { answer: 'b. 삼성전자', score: 4 }
+      { answer: 'a. 코스피', score: 0 },
+      { answer: 'b. 삼성전자', score: 5 }
     ]
   },
   {
@@ -51,59 +51,59 @@ const qnaList = [
   {
     q: '8. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 금', score: 1 },
-      { answer: 'b. 미국 국채', score: 2 }
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 미국 국채', score: 5 }
     ]
   },
   {
     q: '9. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 미국 국채', score: 2 },
+      { answer: 'a. 미국 국채', score: 0 },
       { answer: 'b. 나스닥', score: 5 }
     ]
   },
   {
     q: '10. 둘 중 하나만 투자 가능하다면?',
     a: [
-      { answer: 'a. 금', score: 1 },
-      { answer: 'b. 비트코인', score: 8 }
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 비트코인', score: 5 }
     ]
   }
 ]
 
 const infoList = [
   {
-    from: 18,
-    to: 20,
+    from: 0,
+    to: 9,
     mLeft: '10%',
     name: '채권 몰빵형',
     desc: '주식 100% 몰빵 포트폴리오'
   },
   {
-    from: 21,
-    to: 28,
+    from: 10,
+    to: 24,
     mLeft: '30%',
     name: '채권혼합형',
     desc: '60:40(채권:주식) 포트폴리오'
   },
 
   {
-    from: 24,
-    to: 27,
+    from: 25,
+    to: 30,
     mLeft: '50%',
     name: '뭐든지 반반형',
     desc: '50:50(채권:주식) 포트폴리오'
   },
   {
-    from: 28,
-    to: 33,
+    from: 31,
+    to: 44,
     mLeft: '70%',
     name: '주식혼합형',
     desc: '40:60(채권:주식) 포트폴리오'
   },
   {
-    from: 34,
-    to: 42,
+    from: 45,
+    to: 50,
     mLeft: '90%',
     name: '주식 몰빵형',
     desc: '주식 100% 몰빵 포트폴리오'
