70f35ce9b7e38364b1edaa2a07b22ed314dd74c8
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-12 23:56:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,10 +9,6 @@ const ENDPOINT = 10;
 const select = [];
 let qIdx = -1;
 
-document.querySelector('img').addEventListener('click', function() {
-  document.querySelector('#qna').classList.add('show');
-});
-
 const goTo = (dest) => {
   let elem;
   let elemTop;
